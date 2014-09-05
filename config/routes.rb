acdf8b9086641692a572c068550e1fe478ab062f
Zachary Lovin <zlovin@Zacharys-MacBook-Pro.local>
2014-09-05 13:58:17 -0500

    Signup-progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 SampleApp::Application.routes.draw do
   resources :users
-  get "users/new"
   root 'static_pages#home'
   match '/help',	to: 'static_pages#help',	via: 'get'
   match '/about',	to: 'static_pages#about',	via: 'get'
